f631fe9ce2a717348f9132ad7986dc58c71388b2
Jiahua Cui <jiahua@brainboxlabs.com>
2024-07-27 16:12:29 -0400

    extracted form partial, created edit and update route and controller method

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -13,6 +13,10 @@ class BlogPostsController < ApplicationController
     @blog_post = BlogPost.new
   end
 
+  def edit
+    @blog_post = BlogPost.find(params[:id])
+  end
+
   def create
     @blog_post = BlogPost.new(blog_post_params)
     @blog_post.save
@@ -20,6 +24,16 @@ class BlogPostsController < ApplicationController
       redirect_to @blog_post
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @blog_post = BlogPost.find(params[:id])
+    @blog_post.save
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
