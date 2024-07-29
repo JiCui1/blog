db60a183ffe0cb9dd51752b324d543af14fd7426
Jiahua Cui <jiahua@brainboxlabs.com>
2024-07-29 08:44:58 -0400

    added destroy route and updated route to use resources

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,11 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
@@ -14,7 +15,6 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
 
   def create
@@ -28,10 +28,16 @@ class BlogPostsController < ApplicationController
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
-    @blog_post.save
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @blog_post.destroy
+      redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
     end
@@ -40,5 +46,11 @@ class BlogPostsController < ApplicationController
   private
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
