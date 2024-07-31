d51232d3070505c36a48342fc90a8c945f531199
Jiahua Cui <jiahua@brainboxlabs.com>
2024-07-31 09:14:00 -0400

    added tailwind

--- config/puma.rb
+++ config/puma.rb
@@ -22,7 +22,7 @@ end
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+# Specifies the `port` that Puma will listen on to receive requests; default is 8000.
 port ENV.fetch("PORT") { 8000 }
 
 # Specifies the `environment` that Puma will run in.
